abcf6e0213ac44b75a1dd82adb7c14a3b23708e1
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-03-27 20:22:21 +0530

    Initial Commit

---
+++ ElecXa/src/main/java/com/elecxa/ElecXa/ElecXaApplication.java
@@ -0,0 +1,13 @@
+package com.elecxa.ElecXa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecXaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecXaApplication.class, args);
+	}
+
+}
---
+++ ElecXa/src/test/java/com/elecxa/ElecXa/ElecXaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elecxa.ElecXa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecXaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
